903fbbaf036deb740757a61c208796681343c3e2
Mateusz Popielec <pingwin.sniegus@gmail.com>
2025-01-09 21:11:24 +0100

    Patch 4

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 from flask import Flask, render_template, request, session, redirect, url_for
 
 app = Flask(__name__)
@@ -34,3 +35,142 @@ def str_start():
 
 def run_flask():
     app.run()
+=======
+from flask import Flask, render_template, request, session, redirect, url_for, Response
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.dates import DateFormatter
+import io
+import json
+
+app = Flask(__name__)
+with open('../secret_key.txt', 'r') as file:
+    app.secret_key = file.read().strip()
+CSV_FILE = "sensor_data.csv"
+JSON_FILE = "client_data.json"
+
+
+def load_data(): # funkcja otwiera plik CSV, prasuje dane i zapisuje je do Dataframe
+    try: # otworzenie danych z dataframe oraz sprasowanie daty
+        df = pd.read_csv(CSV_FILE, header=0)  # Wzięcie pod uwagę nagłówków z danymi
+        df["Time"] = pd.to_datetime(df["Time"], format="%Y:%m:%d:%H:%M:%S", errors="coerce")
+
+        # Powiadomienie w konsoli, jeśli dane są niepoprawne
+        if df["Time"].isnull().any():
+            print("Uwaga: Niektóre daty nie zostały poprawnie sparsowane. Sprawdź format danych.")
+
+        return df.dropna()  # Usunięcie błędnych wierszy, jeśli występują
+    except Exception as e: # zawarcie wyjątku na wypadek błędu odczytywania danych
+        print(f"Nie udało się wczytać danych: {e}")
+        return pd.DataFrame()  # Zwrócony zostaje pusty DataFrame w razie błędu
+
+def save_data(temerature, humidity, light): # otworzenie pliku JSON i zapisanie danych
+    with open(JSON_FILE, "w") as f:
+        t_data =  dict(temperature=temerature, humidity=humidity, light=light)
+        json.dump(t_data, f) # nadpisanie danych
+
+
+
+@app.route('/centrum', methods=['GET', 'POST']) # strona gdzie użytkownik ma możliwość dostosowania parametrów
+def index():
+    if request.method == 'POST': # odczytanie danych ze strony
+        temp = request.form.get('slider_temp')
+        humandity = request.form.get('slider_humandity')
+        light = True if request.form.get('light') == '1' else False
+
+        #wyświetlenie danych i przypisanie ich do sessions oraz zapisano do pliku po przez funkcje save_data()
+        print(temp, humandity, light)
+        save_data(temp, humandity, light)
+
+        session['temp'] = temp
+        session['humandity'] = humandity
+        session['light'] = light
+
+        return redirect(url_for('result'))
+
+    return render_template('sliders.html') # wygenerowanie strony
+
+
+# strona internetowa, która wyświetla parametry użytkownika oraz daje możliwość powrotu do menu
+@app.route('/results', methods=['GET','POST'])
+def result(): # odczytanie danych z kontenera sessions
+    temp = session.get('temp')
+    humandity = session.get('humandity')
+    light = session.get('light')
+
+    # wygenerowanie strony, która jest jako wyświetlnie parametrów
+    return render_template('results.html', slider_temp=temp, slider_humandity=humandity, checkbox_light=light)
+
+@app.route("/plot_temp.png") # utworzenie obrazu png, aby była możliwosć wyświetlić na stronie wykres
+def plot_temp(): #odczytanie danych z dataframe
+    df = load_data()
+    if df.empty:
+        return Response(status=204)
+
+    fig, ax = plt.subplots(figsize=(10, 6))
+
+    # Rysowanie danych na wykresie
+    ax.plot(df["Time"], df["Temperature"], label="Temperatura", color="red")
+    ax.set_title("Temperatura", color="#1b5e20")
+    ax.set_xlabel("Czas (Dzień godzina:minuta)")
+    ax.set_ylabel("°C")
+    ax.set_facecolor('#e8f5e9')  # Tło osi (obszar wykresu)
+    fig.patch.set_facecolor('#e8f5e9')  # Tło całego wykresu
+
+    ax.legend()
+    ax.grid()
+
+    # Formatowanie osi czasu
+    date_format = DateFormatter("%m-%d %H:%M") # Format: dzień godzina:minuta
+    ax.xaxis.set_major_formatter(date_format)
+    fig.autofmt_xdate()  # Obrócenie etykiet dla lepszej czytelności
+
+    # Zapis wykresu do strumienia jako PNG
+    img = io.BytesIO()
+    plt.savefig(img, format="png")
+    img.seek(0)
+    plt.close(fig)  # Zamknięcie wykresu w pamięci
+
+    return Response(img.getvalue(), mimetype="image/png") # zwrócenie jako obraz png
+
+
+@app.route("/plot_humidity.png") # utworzenie obrazu png, aby była możliwosć wyświetlić na stronie wykres
+def plot_humidity(): #odczytanie danych z dataframe
+    df = load_data()
+    if df.empty:
+        return Response(status=204)
+
+    # Rysowanie danych na wykresie
+    fig, ax = plt.subplots(figsize=(10, 6))
+    ax.plot(df["Time"], df["Humidity"], label="Wilgotnosc gleby", color="blue")
+    ax.set_title("Wilgotnosc gleby", color="#1b5e20")
+    ax.set_xlabel("Czas (Dzień godzina:minuta)")
+    ax.set_ylabel("Wartość")
+
+    ax.set_facecolor('#e8f5e9')  # Tło osi (obszar wykresu)
+    fig.patch.set_facecolor('#e8f5e9')  # Tło całego wykresu
+
+    ax.legend()
+    ax.grid()
+
+    # Formatowanie osi czasu
+    date_format = DateFormatter("%m-%d %H:%M")  # Format: dzień godzina:minuta
+    ax.xaxis.set_major_formatter(date_format)
+    fig.autofmt_xdate()  # Obrócenie etykiet dla lepszej czytelności
+
+    # Zapis wykresu do strumienia jako PNG
+    img = io.BytesIO()
+    plt.savefig(img, format="png")
+    img.seek(0)
+    plt.close(fig)  # Zamknięcie wykresu w pamięci
+
+    return Response(img.getvalue(), mimetype="image/png")
+
+@app.route("/plots") # strona wyświetlająca wygenerowane wykresy
+def plots():
+    return render_template("plots.html")
+
+@app.route('/') # strona będąca menu aplikacji
+def str_start():
+    return render_template('menu.html')
+>>>>>>> Stashed changes
--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import socket
 import sys
 
@@ -27,3 +28,94 @@ def start_server():
 
         # Zamknięcie połączenia z klientem
         client_socket.close()
+=======
+import socket
+import csv
+import time
+import json
+from datetime import datetime, timedelta
+
+CSV_FILE = "sensor_data.csv"
+JSON_FILE = "client_data.json"
+
+def clear_data(csv_file):
+    try:
+        old_date = datetime.now() - timedelta(days=3)
+
+        data = []
+        with open(csv_file, mode = "r" , newline = "") as file:
+            read = csv.reader(file)
+            head = next(read)
+            for row in read:
+                try:
+                    row_time = datetime.strptime(row[0], "%Y:%m:%d:%H:%M:%S")
+                    if row_time >= old_date:
+                        data.append(row)
+                except ValueError:
+                    continue
+
+
+        with open(csv_file, mode="w", newline="") as file:
+                writer = csv.writer(file)
+                writer.writerow(head)
+                writer.writerows(data)
+    except FileNotFoundError:
+        print(f"Plik {csv_file} nie istnieje.")
+
+def start_server():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(('0.0.0.0', 3000))
+    server_socket.listen(5)
+    print("Serwer nasłuchuje na porcie 3000...")
+
+    try:
+        with open(CSV_FILE, mode="x", newline="") as file:
+            writer = csv.writer(file)
+            writer.writerow(["Time", "Temperature", "Humidity"])
+    except FileExistsError:
+        pass
+
+    while True:
+        client_socket, addr = server_socket.accept()
+        print(f"Połączono z {addr}")
+
+        try:
+            data = client_socket.recv(1024).decode('utf-8')
+            if data:
+                print(f"Otrzymano dane: {data}")
+                try:
+                    params = dict(param.split("=") for param in data.split("&"))
+                    temperature = params.get("temperature", "0")
+                    humidity = params.get("humidity", "0")
+                except ValueError:
+                    print("Błąd: niepoprawny format danych.")
+                    temperature = "0"
+                    humidity = "0"
+
+                t = time.localtime()
+                ttime = time.strftime("%Y:%m:%d:%H:%M:%S", t)
+                with open(CSV_FILE, mode="a", newline="") as file:
+                    writer = csv.writer(file)
+                    writer.writerow([ttime, temperature, humidity])
+
+                try:
+                    with open(JSON_FILE, mode="r") as file:
+                        json_data = json.load(file)
+                        t_temp = json_data.get("temperature", "0")
+                        t_hum = json_data.get("humidity", "0")
+                        t_light = 1 if json_data.get("light", False) else 0
+                    message = t_temp + '|' + t_hum + '|' + str(t_light)
+                    print(f"Wysłanie wiadomości: {message}")
+                    client_socket.send(message.encode('utf-8'))
+                except (FileNotFoundError, json.JSONDecodeError) as e:
+                    print(f"Błąd wczytywania JSON: {e}")
+        except ConnectionResetError:
+            print("Klient zakończył połączenie gwałtownie.")
+        finally:
+            client_socket.close()
+            clear_data(CSV_FILE)
+
+if __name__ == "__main__":
+    start_server()
+
+>>>>>>> Stashed changes
