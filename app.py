344bbc98e886bd18a08476f079a18490c0d181c3
Pugslay <53711206+Pugslay@users.noreply.github.com>
2025-04-05 23:40:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,41 +1,3 @@
-<<<<<<< Updated upstream
-from flask import Flask, render_template, request, session, redirect, url_for
-
-app = Flask(__name__)
-app.secret_key = 'P0L1T36HN1K4'
-
-@app.route('/centrum', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        temp = request.form.get('slider_temp')
-        humandity = request.form.get('slider_humandity')
-        light = True if request.form.get('light') == '1' else False
-
-        session['temp'] = temp
-        session['humandity'] = humandity
-        session['light'] = light
-
-        print(temp, humandity, light)
-
-        return redirect(url_for('result'))
-
-    return render_template('sliders.html')
-
-
-@app.route('/results', methods=['GET','POST'])
-def result():
-    temp = session.get('temp')
-    humandity = session.get('humandity')
-
-    return render_template('results.html', slider_temp=temp, slider_humandity=humandity)
-@app.route('/')
-def str_start():
-    return render_template('menu.html')
-
-
-def run_flask():
-    app.run()
-=======
 from flask import Flask, render_template, request, session, redirect, url_for, Response
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -173,4 +135,3 @@ def plots():
 @app.route('/') # strona będąca menu aplikacji
 def str_start():
     return render_template('menu.html')
->>>>>>> Stashed changes
